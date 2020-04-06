e2c20fee6aae4c6f99e3d756b9e3dda353b2fecc
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-06 16:07:43 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,7 +1,7 @@
 class SightingsController < ApplicationController
   def show
     sighting = Sighting.all
-      render json: sighting.to_json(:include => {
+    render json: sighting.to_json(:include => {
     :bird => {:only => [:name, :species]},
     :location => {:only => [:latitude, :longitude]}
   }, :except => [:updated_at])
